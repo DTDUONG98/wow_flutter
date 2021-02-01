0585868d12ea0a5a7e7eb34df38695208aac32b3
DTDUONG98 <dotienduongptit@gmail.com>
2021-02-01 15:47:20 +0700

    update token

--- lib/data/world_of_warcraft/source/remote/world_of_warcraft_api.dart
+++ lib/data/world_of_warcraft/source/remote/world_of_warcraft_api.dart
@@ -11,17 +11,20 @@ import 'package:retrofit/http.dart';
 
 part 'world_of_warcraft_api.g.dart';
 
+// lấy access_token : curl -u 6bf1d0cf679b432f900c7dd09a06a636:Qz1f0VXXxmRZIpu476LQpYktTO2zAc74 -d grant_type=client_credentials https://us.battle.net/oauth/token
+
+
 @RestApi(baseUrl:'https://us.api.blizzard.com/data/wow/')
 abstract class WOWApi
 {
   factory WOWApi(Dio dio,{String baseUrl}) = _WOWApi;
-  @GET('pvp-season/{pvpSeasonId}/pvp-leaderboard/{pvpBracket}?namespace=dynamic-us&locale=en_US&access_token=USOnWn5Ma0l3uGvCPCOovNfn8HtdzAJfDh')
+  @GET('pvp-season/{pvpSeasonId}/pvp-leaderboard/{pvpBracket}?namespace=dynamic-us&locale=en_US&access_token=USvcxm0hn9dEx6XR8J6cjW0rprbM5Q7cFp')
   Future<LeaderBoard> getPvPRank(@Path('pvpSeasonId') int SeasonId, @Path('pvpBracket') String Bracket );
-  @GET('pet/index?namespace=static-us&locale=en_US&access_token=USOnWn5Ma0l3uGvCPCOovNfn8HtdzAJfDh')
+  @GET('pet/index?namespace=static-us&locale=en_US&access_token=USvcxm0hn9dEx6XR8J6cjW0rprbM5Q7cFp')
   Future<ListPet> getPet();
-  @GET('pet/{petId}?namespace=static-us&locale=en_US&access_token=USOnWn5Ma0l3uGvCPCOovNfn8HtdzAJfDh')
+  @GET('pet/{petId}?namespace=static-us&locale=en_US&access_token=USvcxm0hn9dEx6XR8J6cjW0rprbM5Q7cFp')
   Future<PetDetail> searchPet(@Path('petId') int petId);
-  @GET('quest/{questId}?namespace=static-us&locale=en_US&access_token=USOnWn5Ma0l3uGvCPCOovNfn8HtdzAJfDh')
+  @GET('quest/{questId}?namespace=static-us&locale=en_US&access_token=USvcxm0hn9dEx6XR8J6cjW0rprbM5Q7cFp')
   Future<Quests> SearchQuest(@Path('questId') int questId);
 
 }
